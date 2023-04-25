d00099dcbcb7c5dba6754e22a3e7232bdce3275b
LongPhi <nguyenphilongls2k4@gmail.com>
2023-04-25 14:26:53 +0700

    long topbar

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container,Row,Col} from "react-bootstrap";
+
+import Topbar from './components/topbar';
 function App() {
   return (
    <>
          {/* TOP NAVIGATION -PHI LONG */}
-      <h1>import no vao day nhha Long</h1>
+      <Topbar/>
      
         {/* BOT NAVIGATION -PHUONG ANH */}
-      <h1>import no vao day nha anh</h1>
+      <h1 className='text'>import no vao day nha anh </h1>
      
-
       {/* Sidebar TRUNG  NGUYEN */}
-      
-      </>
+    </>  
 
   );
 }
