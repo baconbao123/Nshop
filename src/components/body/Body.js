940d14b46cb34999fdda1c96dd9767e34800a1b8
LongPhi <nguyenphilongls2k4@gmail.com>
2023-04-27 23:06:02 +0700

    Long cập nhật Body.js < thêm phần slide trong body>

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Container,Row,Col} from 'react-bootstrap'
 import Slidebar from '../sildebar/Slidebar'
+import CarouselSlide from '../carousel/carousel'
+import './body.css'
 export default function Body() {
   return (
-    <Container>
+    <Container className='body'>
         <Row>
         <Col md={3}>
           <Slidebar></Slidebar>
         </Col>
         <Col md={9}>
-          Truogn trung nguyen
+          {/* Truogn trung nguyen */}
+          <CarouselSlide/>
         </Col>
         </Row>
     </Container>
