4983b3ab0f1258a2f6cb533b90b14cf98aee8852
Yahbini <ltpa1203@gmail.com>
2023-04-27 23:59:24 +0700

    pa gui card

---
+++ src/components/Content/content.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { Card } from "react-bootstrap"
+
+const productNew = [
+    {
+		img:  "https://nshopvn.com/wp-content/uploads/2023/04/den-canh-bao-xoay-vong-halogen-mau-do-12v-9wnn-1-300x300.jpg",
+		content: "Đèn cảnh báo xoay vòng Halogen màu Đỏ 12V",
+		price: "208.000₫"
+	},
+	{
+		img: "https://nshopvn.com/wp-content/uploads/2023/04/den-canh-bao-xoay-vong-halogen-mau-do-24v-6l3h-1-300x300.jpg",
+		content: "Đèn cảnh báo xoay vòng Halogen màu Đỏ 24V",
+		price: "208.000₫"
+	},
+] 
+
+const productFeatured = [
+    {
+		img:  "https://nshopvn.com/wp-content/uploads/2023/04/den-canh-bao-xoay-vong-halogen-mau-do-12v-9wnn-1-300x300.jpg",
+		content: "Đèn cảnh báo xoay vòng Halogen màu Đỏ 12V",
+		price: "208.000₫"
+	},
+	{
+		img: "https://nshopvn.com/wp-content/uploads/2023/04/den-canh-bao-xoay-vong-halogen-mau-do-24v-6l3h-1-300x300.jpg",
+		content: "Đèn cảnh báo xoay vòng Halogen màu Đỏ 24V",
+		price: "208.000₫"
+	},
+] 
+
+const ProductList = () => {
+    return (
+        
+    )
+}
+
+export default ProductList
