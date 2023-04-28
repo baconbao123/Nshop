71d8f2449bca90a629b686fc4ddffbf99aa2cb87
Yahbini <ltpa1203@gmail.com>
2023-04-28 14:34:39 +0700

    pa code card

--- src/components/Content/Card/ProductCard.js
+++ src/components/Content/Card/ProductCard.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import './card.css'
 
-const ProductCard = (products) => {
-    return (
-        {products.map((product,index) => (
-            <Card>
-                <Card.Img variant={index} src={product.img}/>
+const ProductCard = ({ products }) => {
+  return (
+    <>
+        {products.map((product, index) => (
+            <Card key={index} style={{ width: '12rem' }} className="card">
+                <Card.Img variant={index} src={product.img} />
                 <Card.Body>
-                    <Card.Text>{product.content}</Card.Text>
-                    <Card.Text>{product.price}</Card.Text>
+                <Card.Text>{product.content}</Card.Text>
+                <Card.Text>{product.price}</Card.Text>
                 </Card.Body>
             </Card>
-        ))}
-    )
-}
+        ))};
+    </>
+  )
+};
 
-export default ProductCard
+export default ProductCard;
