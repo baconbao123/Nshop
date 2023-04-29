8ecb26cf50509e6fbfaae2f54752cbfed500bbde
LongPhi <nguyenphilongls2k4@gmail.com>
2023-04-29 16:45:47 +0700

    Long cập nhật thêm phần ul và thẻ a trong product card

--- src/components/Content/Card/ProductCard.js
+++ src/components/Content/Card/ProductCard.js
@@ -1,19 +1,28 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-import './card.css'
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+
 
 const ProductCard = ({ products }) => {
   return (
     <>
-        {products.map((product, index) => (
-            <Card key={index} style={{ width: '12rem' }} className="card">
+      {products.map((product, index) => (
+        // <li className="product">
+          <Col md={2} className="product">
+            <a href="#">
+              <Card key={index} style={{ width: '12rem' }} className="card">
                 <Card.Img variant={index} src={product.img} />
                 <Card.Body>
-                <Card.Text>{product.content}</Card.Text>
-                <Card.Text>{product.price}</Card.Text>
+                  <Card.Text className="content-product" title={product.content}>{product.content}</Card.Text>
+                  <Card.Text className="price">{product.price}</Card.Text>
                 </Card.Body>
-            </Card>
-        ))};
+              </Card> 
+          </a>
+          </Col>
+
+        // </li>
+      ))};
     </>
   )
 };
