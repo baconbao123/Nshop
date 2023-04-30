f7dd12b589d2866dd21533edba9d174efe994515
Yahbini <ltpa1203@gmail.com>
2023-04-30 21:15:50 +0700

    pa thêm span SoldOut

--- src/components/Content/Card/ProductCard.js
+++ src/components/Content/Card/ProductCard.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import { Card } from "react-bootstrap";
+import styled from "styled-components";
+
+const SoldOutSpan = styled.span`
+  background-color: #F0F2F5;   
+  color: #8c8c8c;   
+  border: 1px solid #8c8c8c;
+  padding: 5px 10px;    
+  position: absolute;    
+  bottom: 100px;
+  font-size: 14px;
+  margin-left: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 const ProductCard = ({ product }) => {
+  const[isSoldOut, setIsSoldOut] = useState(product.soldOut==="Hết Hàng")
+
   return (
     <>
       <Card style={{ width: '12rem' }} className="card">
+      {isSoldOut && <SoldOutSpan>Hết hàng</SoldOutSpan>}
         <Card.Img src={product.img} />
         <Card.Body>
           <Card.Text className="content-product" title={product.content}>{product.content}</Card.Text>
--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -8,11 +8,13 @@ const productNew = [
 		img: 'https://nshopvn.com/wp-content/uploads/2023/04/den-canh-bao-xoay-vong-halogen-mau-do-12v-9wnn-1-300x300.jpg',
 		content: 'Đèn cảnh báo xoay vòng Halogen màu Đỏ 12V',
 		price: '208.000₫',
+		soldOut: 'Hết Hàng'
 	},
 	{
 		img: 'https://nshopvn.com/wp-content/uploads/2023/04/den-canh-bao-xoay-vong-halogen-mau-do-24v-6l3h-1-300x300.jpg',
 		content: 'Đèn cảnh báo xoay vòng Halogen màu Đỏ 24V',
 		price: '208.000₫',
+		soldOut: 'Hết Hàng'
 	},
 	{
 		img: 'https://nshopvn.com/wp-content/uploads/2023/04/den-canh-bao-xoay-vong-halogen-mau-vang-12v-3br2-1-300x300.jpg',
