9c6c4029c9fc3082a2af829fa3a65d70cbba8e05
Yahbini <ltpa1203@gmail.com>
2023-04-30 11:54:10 +0700

    pa chinh lai card va content

--- src/components/Content/Card/ProductCard.js
+++ src/components/Content/Card/ProductCard.js
@@ -4,25 +4,20 @@ import { Row } from "react-bootstrap";
 import { Col } from "react-bootstrap";
 
 
-const ProductCard = ({ products }) => {
+const ProductCard = ({ product }) => {
   return (
     <>
-      {products.map((product, index) => (
-        // <li className="product">
           <Col md={2} className="product">
             <a href="#">
-              <Card key={index} style={{ width: '12rem' }} className="card">
-                <Card.Img variant={index} src={product.img} />
+              <Card style={{ width: '12rem' }} className="card">
+                <Card.Img src={product.img}/>
                 <Card.Body>
                   <Card.Text className="content-product" title={product.content}>{product.content}</Card.Text>
                   <Card.Text className="price">{product.price}</Card.Text>
                 </Card.Body>
               </Card> 
-          </a>
+            </a>
           </Col>
-
-        // </li>
-      ))};
     </>
   )
 };
