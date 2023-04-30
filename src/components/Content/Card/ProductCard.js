cfe4c62132bf501e7c402f782009080e312d5d42
LongPhi <nguyenphilongls2k4@gmail.com>
2023-04-30 18:58:50 +0700

    Long sửa lại code phần hiển thị danh sách product

--- src/components/Content/Card/ProductCard.js
+++ src/components/Content/Card/ProductCard.js
@@ -1,23 +1,16 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-import { Row } from "react-bootstrap";
-import { Col } from "react-bootstrap";
-
 
 const ProductCard = ({ product }) => {
   return (
     <>
-          <Col md={2} className="product">
-            <a href="#">
-              <Card style={{ width: '12rem' }} className="card">
-                <Card.Img src={product.img}/>
-                <Card.Body>
-                  <Card.Text className="content-product" title={product.content}>{product.content}</Card.Text>
-                  <Card.Text className="price">{product.price}</Card.Text>
-                </Card.Body>
-              </Card> 
-            </a>
-          </Col>
+      <Card style={{ width: '12rem' }} className="card">
+        <Card.Img src={product.img} />
+        <Card.Body>
+          <Card.Text className="content-product" title={product.content}>{product.content}</Card.Text>
+          <Card.Text className="price">{product.price}</Card.Text>
+        </Card.Body>
+      </Card>
     </>
   )
 };
