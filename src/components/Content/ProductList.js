80805e33ea573cefcef96d24dac9087e181b9091
LongPhi <nguyenphilongls2k4@gmail.com>
2023-05-01 15:26:22 +0700

    Long sửa lại key

--- src/components/Content/ProductList.js
+++ src/components/Content/ProductList.js
@@ -4,17 +4,18 @@ import { Col } from 'react-bootstrap';
 
 import Card from './Card/ProductCard';
 const ProductList = ({ products }) => {
+    
     return (
         <div className='product-list'>
             <Row className="card-product">
-                {products.map((product) => (
-
-                    <Col md={2}>
-                        <Card key={product.content} product={product} />
+                {products.map((product,index) => (
+                    <Col md={2} key={index}>
+                        <Card  product={product} />
                     </Col>
                 ))}
             </Row>
         </div>
+        
     )
 }
 
