3ddad218bd838c969cc919e77643f2c1352adfec
LongPhi <nguyenphilongls2k4@gmail.com>
2023-05-15 13:50:32 +0700

    Long update footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,12 +6,16 @@ import { Container } from 'react-bootstrap';
 import dathongbao from '../../img/dathongbao.png'
 import proteced from '../../img/proteced.png'
 import './footer.css'
+
+import BottomNav from '../BottomNav/BottomNav';
+//icon
+
 function footer() {
     return (
         <div className='footer'>
-            <Container>
-                <Row className='w-100 content-footer '>
-                    <Col md={7} className='footer-left'>
+            <Container >
+                <Row className='w-100 content-footer d-lg-flex'>
+                    <Col lg={6} md={7} className='footer-left'>
                         <ul className='ul-footer'>
                             <a href="#/guarantee"> <li>Bảo hành</li></a>
                             <a href="#/doitra"> <li>Đổi trả</li></a>
@@ -21,17 +25,22 @@ function footer() {
                         <div className='content-footer'>
                             <span>Hộ kinh doanh Linh kiện điện tử Nshop / GPĐKKD số: 41X8035261 do UBND Quận Tân Phú cấp ngày 08/05/2019</span>
                             <p> <b> Điện tử NShop Tân Phú:</b> 1 Bùi Xuân Phái, Tây Thạnh, Tân Phú, TP. HCM – 📞 0902 64 39 78 <br />
-                               <b>Điện tử NShop Quận 9:</b>  7 Trần Hưng Đạo, Hiệp Phú, Quận 9, TP. HCM – 📞 093 27 23 186</p>
+                                <b>Điện tử NShop Quận 9:</b>  7 Trần Hưng Đạo, Hiệp Phú, Quận 9, TP. HCM – 📞 093 27 23 186</p>
                             <b>NSHOPVN.COM © 2019 - 2021</b>
                         </div>
                     </Col>
-                    <Col md={6} className='text-center'>
-                        <img src={proteced} width={135} />
-                        <img src={dathongbao} width={180} />
+                    <Col lg={6} md={5} className='text-center'>
+                        <img src={proteced} width={134.72} />
+                        <img src={dathongbao} width={179.63} />
 
                     </Col>
+
                 </Row>
+
             </Container>
+            {/* NavBar responsive md */}
+            <BottomNav/>
+
 
         </div>
     )
