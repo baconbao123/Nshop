b216ee3eed57c47902930e1a0a3714394ebcb140
LongPhi <nguyenphilongls2k4@gmail.com>
2023-05-17 10:20:38 +0700

    Long them componnet category

---
+++ src/components/Category/Category.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+import {Row} from 'react-bootstrap'
+import {Col} from 'react-bootstrap'
+
+//img
+import ardyino from "../../img/iconSildebar/arduino-icon.png"
+import cambien from "../../img/iconSildebar/cam-bien.png"
+import combo from "../../img/iconSildebar/combo.png"
+import denLed from "../../img/iconSildebar/den-led-dieu-khien-den-led.png"
+import dienDanDung from "../../img/iconSildebar/dien-dan-dung-va-cong-nghiep.png"
+import dienNangLuong from "../../img/iconSildebar/dien-nang-luong-mat-troi-solar.png"
+import dongHo from "../../img/iconSildebar/dong-ho-van-nang.png"
+import mayIn from "../../img/iconSildebar/may-in-3d-cong-nghe.png"
+import machDien from "../../img/iconSildebar/module-mach-dien.png"
+import robot from "../../img/iconSildebar/robot-phu-kien-diy.png"
+import phuKien from "../../img/iconSildebar/phu-kien-dung-cu.png"
+import facebook from "../../img/iconSildebar/facebook.png"
+import youtube from "../../img/iconSildebar/youtube.png"
+import group from "../../img/iconSildebar/group.png"
+function Category() {
+  return (
+    <div className='mt-5'>
+        <Row>
+            <Col md={3}>
+                <h1>shf</h1>
+            </Col>
+            <Col md={9}>
+
+            </Col>
+        </Row>
+    </div>
+  )
+}
+
+export default Category
